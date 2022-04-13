1cf8a0f9418e1570d9f36986647bee04ad7396d3
4the4ryushin <aman2001mi@gmail.com>
2022-04-13 10:36:27 +0530

    added article , comments and posts

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  include Visible
+  belongs_to :article
+  validates :commenter,presence: true
+  validates :body, presence: true, length: {minimum:10}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'articles/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'welcome', to: 'welcome#home'
-  get 'about', to: 'welcome#about'
+  root "home#welcome"
+  get 'about', to: 'home#about'
+  resources :articles do
+    resources :comments
+  end
+  # get '/articles', to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
